ed3c2162059478d6d90c14814a752518ae2228f3
MierkiewiczN <natalia.mmie@gmail.com>
2022-12-04 21:34:10 +0100

    added build folder

---
+++ build/precache-manifest.cb25c6acc42d026ca6d0ad663bad8ed2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "30b8c7bb4db9accee4dd",
+    "url": "/static/css/main.ee4a2fe2.chunk.css"
+  },
+  {
+    "revision": "30b8c7bb4db9accee4dd",
+    "url": "/static/js/main.f05ee633.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e4f104a3950d72866601",
+    "url": "/static/css/2.b6d4f7df.chunk.css"
+  },
+  {
+    "revision": "e4f104a3950d72866601",
+    "url": "/static/js/2.57ff765f.chunk.js"
+  },
+  {
+    "revision": "1a817a95a42d8c43031378d122a05ffe",
+    "url": "/static/media/no-avatar.1a817a95.png"
+  },
+  {
+    "revision": "46626d3e95b79b577ae7583bda75b4fb",
+    "url": "/static/media/mapa_poziomy.46626d3e.png"
+  },
+  {
+    "revision": "ffc87b4b921d8ec92e65c4f689673198",
+    "url": "/static/media/coming-soon.ffc87b4b.gif"
+  },
+  {
+    "revision": "1fcf426da104e05fe7e6a53b9c16fe9e",
+    "url": "/static/media/Scratch.1fcf426d.mp4"
+  },
+  {
+    "revision": "73ec23d82c33df29bdafd2d97455137d",
+    "url": "/static/media/logo.73ec23d8.png"
+  },
+  {
+    "revision": "f7ff421861501e099a86614c2a391a1b",
+    "url": "/index.html"
+  }
+];
